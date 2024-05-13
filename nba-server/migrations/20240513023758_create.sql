12cbe67cecaa9fb16ca70f18040904237d52c88c
zhangyemengren <624593758@qq.com>
2024-05-13 13:19:37 +0800

    add team col

--- nba-server/migrations/20240513023758_create.sql
+++ nba-server/migrations/20240513023758_create.sql
@@ -1,10 +1,13 @@
 -- database
-create database nba DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+# create database nba;
+ALTER DATABASE nba CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- tables
-create table team (
-  id int primary key auto_increment,
-  name varchar(255) not null,
-  city varchar(255) not null,
-  abbreviation varchar(255) not null
+CREATE TABLE team
+(
+    id           int primary key auto_increment,
+    name         varchar(255) not null,
+    home_court         varchar(255) not null,
+    abbreviation varchar(255) not null,
+    area       varchar(255) not null
 );
 
---
+++ nba-server/migrations/20240513032654_table_team.sql
@@ -0,0 +1,3 @@
+-- Add migration script here
+INSERT INTO team (name, home_court, abbreviation, area)
+VALUES ('菲尼克斯太阳', '足迹中心', 'Suns', 'West');
