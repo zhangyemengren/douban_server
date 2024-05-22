c797d8ed74f1977e63b2ae1c100040957fd229bd
zhangyemengren <624593758@qq.com>
2024-05-22 18:03:44 +0800

    sql

--- nba-server/migrations/20240513023758_create.sql
+++ nba-server/migrations/20240513023758_create.sql
@@ -8,6 +8,7 @@ CREATE TABLE team
     name         varchar(255) not null,
     home_court         varchar(255) not null,
     abbreviation varchar(255) not null,
-    area       varchar(255) not null
+    area       varchar(255) not null,
+    `partition`  varchar(255) not null
 );
 
