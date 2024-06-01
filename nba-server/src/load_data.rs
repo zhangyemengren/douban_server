b3ddc5cfea12e23d02abc434462f1cc378fd7957
zhangyemengren <624593758@qq.com>
2024-06-01 23:12:36 +0800

    update

--- nba-server/src/load_data.rs
+++ nba-server/src/load_data.rs
@@ -1,12 +1,28 @@
 use std::env;
 use reqwest;
-use serde_json::Value;
+use serde::{Deserialize, Serialize};
+// use serde_json::{Deserializer, Serializer};
+
+#[derive(Debug,Deserialize,Serialize)]
+pub struct Team {
+    id: i32,
+    abbreviation: String,
+    city: String,
+    conference: String,
+    division: String,
+    full_name: String,
+    name: String
+}
+#[derive(Debug,Deserialize,Serialize)]
+pub struct Data {
+    data: Vec<Team>
+}
 
 pub async fn load(){
     let key = env::var("API_KEY").unwrap();
     let url = "https://api.balldontlie.io/v1/teams";
     let client = reqwest::Client::new();
-    let res: Value = client.get(url)
+    let res: Data = client.get(url)
         .header("Authorization", key).send().await.unwrap().json().await.unwrap();
     println!("{:?}", res);
 }
