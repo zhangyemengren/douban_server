4161070a51ac825064ca535df73a7a289767c37d
zhangyemengren <624593758@qq.com>
2024-06-20 16:48:09 +0800

    update

--- nba-server/src/data/team.rs
+++ nba-server/src/data/team.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::fmt::Display;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Team {
@@ -16,4 +17,14 @@ pub struct Team {
 pub enum Conference {
     East,
     West,
+}
+
+impl Display for Conference {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = match self {
+            Conference::East => "east".to_string(),
+            Conference::West => "west".to_string(),
+        };
+        write!(f, "{}", str)
+    }
 }
--- nba-server/src/routes/team.rs
+++ nba-server/src/routes/team.rs
@@ -1,5 +1,8 @@
 use crate::data::{AppState, Conference, Team};
-use axum::{extract::{State, Path}, Json};
+use axum::{
+    extract::{Path, State},
+    Json,
+};
 
 pub async fn get_team(State(AppState { pool, .. }): State<AppState>) -> Json<Vec<Team>> {
     let teams = sqlx::query_as!(
@@ -14,16 +17,19 @@ pub async fn get_team(State(AppState { pool, .. }): State<AppState>) -> Json<Vec
     teams.into()
 }
 
-pub async fn get_team_by_conference(Path(c): Path<Conference>, State(AppState { pool, .. }): State<AppState>) -> Json<Vec<Team>> {
+pub async fn get_team_by_conference(
+    Path(c): Path<Conference>,
+    State(AppState { pool, .. }): State<AppState>,
+) -> Json<Vec<Team>> {
     let teams = sqlx::query_as!(
         Team,
         r#"
             SELECT * FROM team where conference = ?
             "#,
-            "east"
+        c.to_string()
     )
-        .fetch_all(&pool)
-        .await
-        .unwrap_or(vec![]);
+    .fetch_all(&pool)
+    .await
+    .unwrap_or(vec![]);
     teams.into()
 }
